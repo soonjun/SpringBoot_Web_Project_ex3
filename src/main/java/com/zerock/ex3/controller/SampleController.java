f13e3d106497e1fb41b09f4cd1416b106054d373
Sunjun Ok <remedime0@gmail.com>
2022-10-04 02:57:19 +0900

    Initial commit

---
+++ src/main/java/com/zerock/ex3/controller/SampleController.java
@@ -0,0 +1,41 @@
+package com.zerock.ex3.controller;
+
+import com.zerock.ex3.dto.SampleDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/sample")
+@Slf4j
+public class SampleController {
+
+    @GetMapping("/ex1")
+    public void ex1() {
+
+        log.info("ex1..........");
+    }
+
+    @GetMapping({"/ex2"})
+    public void exModel(Model model) {
+
+        List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i->{
+            SampleDTO dto = SampleDTO.builder()
+                    .sno(i)
+                    .first("First.."+i)
+                    .last("Last.."+i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+            return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("list", list);
+    }
+}
